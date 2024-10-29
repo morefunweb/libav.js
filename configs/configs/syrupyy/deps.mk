6686538d6bd5dc74524b00b2d49a7c838757ce86
illbloo <152131832+illbloo@users.noreply.github.com>
2024-10-28 21:03:57 -0700

    Add Comic Studio configs

---
+++ configs/configs/syrupyy/deps.mk
@@ -0,0 +1,6 @@
+build/ffmpeg-$(FFMPEG_VERSION)/build-base-webm-mp4/ffbuild/config.mak: build/inst/base/lib/pkgconfig/opus.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-thr-webm-mp4/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/opus.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-base-webm-mp4/ffbuild/config.mak: build/inst/base/lib/pkgconfig/vpx.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-thr-webm-mp4/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/vpx.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-base-webm-mp4/ffbuild/config.mak: build/inst/base/lib/pkgconfig/openh264.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-thr-webm-mp4/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/openh264.pc
