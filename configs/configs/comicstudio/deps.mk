cf5e43ecf8fc744c6ad26882cc55073f7d9dde62
illbloo <152131832+illbloo@users.noreply.github.com>
2024-10-29 00:14:00 -0700

    More fixes

--- configs/configs/comicstudio/deps.mk
+++ configs/configs/comicstudio/deps.mk
@@ -1,4 +1,4 @@
-build/ffmpeg-$(FFMPEG_VERSION)/build-base-webm-aac/ffbuild/config.mak: build/inst/base/lib/pkgconfig/opus.pc
-build/ffmpeg-$(FFMPEG_VERSION)/build-thr-webm-aac/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/opus.pc
-build/ffmpeg-$(FFMPEG_VERSION)/build-base-webm-aac/ffbuild/config.mak: build/inst/base/lib/pkgconfig/vpx.pc
-build/ffmpeg-$(FFMPEG_VERSION)/build-thr-webm-aac/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/vpx.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-base-comicstudio/ffbuild/config.mak: build/inst/base/lib/pkgconfig/opus.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-thr-comicstudio/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/opus.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-base-comicstudio/ffbuild/config.mak: build/inst/base/lib/pkgconfig/vpx.pc
+build/ffmpeg-$(FFMPEG_VERSION)/build-thr-comicstudio/ffbuild/config.mak: build/inst/thr/lib/pkgconfig/vpx.pc
